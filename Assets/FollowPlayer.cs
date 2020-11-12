0605d7244ad747e903f7cda84d52d11ad6f0e51e
Tulio de Souza <ss.tulio@gmail.com>
2020-11-11 21:30:19 -0300

    initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newXPosition = Player.transform.position.x - offset.x;
+        float newZPosition = Player.transform.position.z;
+
+        transform.position = new Vector3(newXPosition, transform.position.y, newZPosition - 5);
+    }
+}
---
+++ Assets/ThirdPersonInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonInput : MonoBehaviour
+{
+
+    public FixedJoystick LeftJoystick;
+    protected ThirdPersonUserControl Control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Control = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control.Hinput = LeftJoystick.Direction.x;
+        Control.Vinput = LeftJoystick.Direction.y;
+    }
+}
